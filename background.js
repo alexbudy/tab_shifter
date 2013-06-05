edbd64e996cc1fe8fb5f174b47ee10b34611c7f2
Alex Budilovsky <alexbudy@gmail.com>
2013-06-04 23:10:29 -0700

    working tab shifter, version 1

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log('Turning ' + tab.url + 'red!');
-    chrome.tabs.executeScript({
-        code: 'document.body.style.backgroundColor="red"'
-    });
-});
+console.log("in background")
+
+chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+          chrome.tabs.query({'currentWindow': true}, function(tabs) {
+            var tabCount = tabs.length
+            var activeTab
+            for (var i = 0; i < tabCount; i++) {
+                if (tabs[i].active == true) {
+                    activeTab = tabs[i]
+                    break
+                }
+            }
+            var tabId = activeTab.id
+            var tabPos = activeTab.index
+            var newIndex = tabPos
+            switch(request.direction) {
+                case 'right':
+                    newIndex++
+                    if (newIndex >= tabCount) newIndex = 0
+                    break
+                case 'left':
+                    newIndex--
+                    if (newIndex < 0) newIndex = (tabCount - 1)
+                    break 
+            }
+            chrome.tabs.move(tabId, {index:newIndex});
+          })}
+)
--- inject.js
+++ inject.js
@@ -1,5 +1,34 @@
+/*
 chrome.extension.sendRequest({
     "greeting": "hello",
     "var1": "variable 1",
     "var2": true
 });
+*/
+console.log("INJECTED")
+
+window.addEventListener('keyup', moveTab, false)
+
+function moveTab(e) 
+{
+    var RIGHT = 39
+    var LEFT = 37
+    var direction 
+    switch(e.which) {
+         case RIGHT:
+            direction = "right"
+            break
+         case LEFT:
+            direction = "left"
+            break            
+     } 
+    console.log(e)
+
+    if (e.ctrlKey && direction) {
+        console.log("sending request to move tab " + direction)
+        chrome.extension.sendRequest({
+            "request" : "moveTab",
+            "direction" : direction
+        })
+    }
+}
